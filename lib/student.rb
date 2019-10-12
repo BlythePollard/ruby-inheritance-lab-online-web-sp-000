8a26b9be891ead9c9d418db123513e077d45ffd4
Blythe Pollard <blytheepollard@gmail.com>
2019-10-12 20:51:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
 class Student < User
+  @knowledge = []
   
+  def initialize
+    @knowledge << self
+  end
+  
+  def learn(string)
+    @knowledge << string
+  end
+  
+  def knowledge
+    @knowledge
+  end
 end
